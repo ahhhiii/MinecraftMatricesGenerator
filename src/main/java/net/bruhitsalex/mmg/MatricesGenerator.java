b5573b3550bafbc048d97895274de27553d7823b
alexh <alex.hammonnd003@gmail.com>
2022-04-28 20:25:25 +0100

    Initial commit

---
+++ src/main/java/net/bruhitsalex/mmg/MatricesGenerator.java
@@ -0,0 +1,10 @@
+package net.bruhitsalex.mmg;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(name = "MatricesGenerator", modid = "mmg", version = "1.0", acceptedMinecraftVersions = "1.8.9")
+public class MatricesGenerator {
+
+
+
+}
