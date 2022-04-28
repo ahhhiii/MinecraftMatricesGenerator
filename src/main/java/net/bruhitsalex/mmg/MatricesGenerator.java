8acb2710315b47fcdbaf9c6c5d977d73f33f53de
alexh <alex.hammonnd003@gmail.com>
2022-04-28 21:23:09 +0100

    Main Class

--- src/main/java/net/bruhitsalex/mmg/MatricesGenerator.java
+++ src/main/java/net/bruhitsalex/mmg/MatricesGenerator.java
@@ -1,10 +1,17 @@
 package net.bruhitsalex.mmg;
 
+import net.bruhitsalex.mmg.manager.MMGManager;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(name = "MatricesGenerator", modid = "mmg", version = "1.0", acceptedMinecraftVersions = "1.8.9")
 public class MatricesGenerator {
 
+    @Mod.EventHandler
+    public void onInit(FMLInitializationEvent event) {
+        MMGManager.init();
 
+
+    }
 
 }
