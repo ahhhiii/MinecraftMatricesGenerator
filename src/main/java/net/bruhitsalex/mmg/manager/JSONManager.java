258c7c0847ca0c9b3a6c878b36d122ae3a1d1db9
alexh <alex.hammonnd003@gmail.com>
2022-04-28 21:23:02 +0100

    Created class to write data to json file

---
+++ src/main/java/net/bruhitsalex/mmg/manager/JSONManager.java
@@ -0,0 +1,67 @@
+package net.bruhitsalex.mmg.manager;
+
+import com.google.gson.*;
+import net.bruhitsalex.mmg.utils.ChatUtils;
+import net.minecraft.util.BlockPos;
+
+import java.awt.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.List;
+
+public class JSONManager {
+
+    public static boolean writeToJson(List<Point> list) {
+        JsonObject json = new JsonObject();
+        list.forEach(point -> {
+            json.add(point.getName(), getPointToJson(point));
+        });
+
+        File tempFile;
+        try {
+            tempFile = File.createTempFile("matrices", ".json");
+        } catch (Exception e) {
+            e.printStackTrace();
+            ChatUtils.sendMessage("Unable to create file to store data.. Check your minecraft output log.");
+            return false;
+        }
+
+        try {
+            new GsonBuilder().setPrettyPrinting().create().toJson(json, new FileWriter(tempFile));
+            ChatUtils.sendMessage("Written to file: " + tempFile.getAbsolutePath());
+            if (Desktop.isDesktopSupported()) {
+                Desktop.getDesktop().open(tempFile);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            ChatUtils.sendMessage("Unable to write json to file.. Writing in your output log (find in .minecraft/logs/)");
+            System.out.println(json);
+            return false;
+        }
+
+        return true;
+    }
+
+    public static JsonObject getPointToJson(Point point) {
+        JsonObject json = new JsonObject();
+        json.addProperty("name", point.getName());
+        json.add("position", getPosToJson(point.getPosition()));
+
+        JsonArray array = new JsonArray();
+        for (Point point1 : point.getConnectedTo()) {
+            array.add(new JsonPrimitive(point1.getName()));
+        }
+
+        json.add("connectedTo", array);
+        return json;
+    }
+
+    public static JsonObject getPosToJson(BlockPos pos) {
+        JsonObject json = new JsonObject();
+        json.addProperty("x", pos.getX());
+        json.addProperty("y", pos.getY());
+        json.addProperty("z", pos.getZ());
+        return json;
+    }
+
+}
