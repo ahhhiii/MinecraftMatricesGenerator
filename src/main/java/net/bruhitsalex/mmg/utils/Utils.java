7b13e9773e4b0151073b74b7e9a9b0b4b4256d07
alexh <alex.hammonnd003@gmail.com>
2022-04-29 02:12:00 +0100

    Added rounding method to make positions integer

--- src/main/java/net/bruhitsalex/mmg/utils/Utils.java
+++ src/main/java/net/bruhitsalex/mmg/utils/Utils.java
@@ -1,5 +1,8 @@
 package net.bruhitsalex.mmg.utils;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.util.BlockPos;
+
 public class Utils {
 
     public static String toAlphabetic(int i) {
@@ -15,6 +18,10 @@ public class Utils {
         } else {
             return toAlphabetic(quot-1) + letter;
         }
+    }
+
+    public static BlockPos round(Entity entity) {
+        return new BlockPos((int) entity.posX, (int) entity.posY, (int) entity.posZ);
     }
 
 }
