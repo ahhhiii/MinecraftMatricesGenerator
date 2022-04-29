066e3b4496b151c887b4bf4253a2e83edc1082ad
alexh <alex.hammonnd003@gmail.com>
2022-04-29 02:11:47 +0100

    Removed useless settings/constructors

--- src/main/java/net/bruhitsalex/mmg/manager/Point.java
+++ src/main/java/net/bruhitsalex/mmg/manager/Point.java
@@ -1,21 +1,17 @@
 package net.bruhitsalex.mmg.manager;
 
-import lombok.*;
+import lombok.Getter;
 import net.minecraft.util.BlockPos;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@RequiredArgsConstructor
 public class Point {
 
-    private String name;
-    private BlockPos position;
-    private List<Point> connectedTo;
+    private final String name;
+    private final BlockPos position;
+    private final List<Point> connectedTo;
 
     public Point(BlockPos position) {
         this.name = MMGManager.getNextName();
