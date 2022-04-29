5c65d50c66bae389f844bd4e0d5fa11eaf09f48c
alexh <alex.hammonnd003@gmail.com>
2022-04-29 02:10:49 +0100

    Fixed base command

--- src/main/java/net/bruhitsalex/mmg/cmds/BaseCommand.java
+++ src/main/java/net/bruhitsalex/mmg/cmds/BaseCommand.java
@@ -1,32 +1,66 @@
 package net.bruhitsalex.mmg.cmds;
 
+import net.bruhitsalex.mmg.cmds.cmds.*;
+import net.bruhitsalex.mmg.manager.MMGManager;
+import net.bruhitsalex.mmg.utils.ChatUtils;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
-import net.minecraftforge.client.ClientCommandHandler;
 
-public abstract class BaseCommand extends CommandBase {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    private final String name;
-    public BaseCommand(String name) {
-        this.name = name;
-        ClientCommandHandler.instance.registerCommand(this);
-    }
+public class BaseCommand extends CommandBase {
 
-    public abstract void onExecute(String[] args);
+    private static final List<Command> COMMANDS = Arrays.asList(
+            new StartCommand(),
+            new FinishCommand(),
+            new AddPointCommand(),
+            new DeletePointCommand(),
+            new AddConnectionCommand()
+    );
 
     @Override
     public String getCommandName() {
-        return name;
+        return "mmg";
     }
 
     @Override
     public String getCommandUsage(ICommandSender sender) {
-        return name;
+        return "/mmg <args>";
     }
 
     @Override
     public void processCommand(ICommandSender sender, String[] args) {
-        onExecute(args);
+        if (args.length == 0) {
+            ChatUtils.sendMessage("You must provide a sub-command: "
+                    + COMMANDS.stream().map(Command::getSubCmd).collect(Collectors.toList()));
+            return;
+        }
+
+        boolean found = false;
+        Command foundCommand = null;
+        for (Command command : COMMANDS) {
+            if (command.getSubCmd().equalsIgnoreCase(args[0])) {
+                found = true;
+                foundCommand = command;
+            }
+        }
+
+        if (!found) {
+            ChatUtils.sendMessage("That sub-command cannot be found, use one of the following: "
+                    + COMMANDS.stream().map(Command::getSubCmd).collect(Collectors.toList()));
+            return;
+        }
+
+        if (foundCommand.isRequiresStarted() && !MMGManager.isActive()) {
+            ChatUtils.sendMessage("This command requires you have started mmg. You can do so with: /mmg " + StartCommand.NAME + ".");
+            return;
+        }
+
+        List<String> newArgs = new ArrayList<>(Arrays.asList(args).subList(1, args.length));
+        foundCommand.onExecute(newArgs);
     }
 
     @Override
